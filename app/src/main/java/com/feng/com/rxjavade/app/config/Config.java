a8e6cfb0ccbe2a0cc4518d9409bde5717de318df
wanghaifeng778s <1695391081@qqcom>
2017-08-21 19:03:55 +0800

    智能刷新框架引入

--- app/src/main/java/com/feng/com/rxjavade/app/config/Config.java
+++ app/src/main/java/com/feng/com/rxjavade/app/config/Config.java
@@ -1,8 +1,15 @@
 package com.feng.com.rxjavade.app.config;
 
+import com.feng.com.rxjavade.BuildConfig;
+
 /**
  * Created by WHF.Javas on 2017/8/21.
  */
 
 public class Config {
+
+    public static final String HOST= BuildConfig.HOST;
+
+    public static final String GET_HOME_PAGE="http://47.91.65.163:17621/recom/doc.do?appid=2&topDocKey=zxTopDoc&devid=";
+
 }
